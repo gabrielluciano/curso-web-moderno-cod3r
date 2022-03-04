26350817e1649d940a3786dc503438eef78071f8
Gabriel Luciano <gabriel-luciano@outlook.com>
2022-03-04 08:57:46 -0300

    Adicionado arquivos do projeto Base Conhecimento

---
+++ projeto-base-conhecimento/backend/config/mongodb.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+    .catch(e => {
+        const msg = "ERRO: Não foi possível conectar com o MongoDB!"
+        console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m');
+    })
---
+++ projeto-base-conhecimento/backend/knexfile.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+  client: 'postgresql',
+  connection: {
+    host :    process.env.DB_HOST,
+    port:     process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user:     process.env.DB_USER,
+    password: process.env.DB_USER_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
